551e300c5149420095bc2a61e39bfa2ac488e6b1
Crowdbotics <team@crowdbotics.com>
2021-08-27 03:32:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fancy_voice_30143Identifier",
+  url: "https://fancy_voice_30143Identifier.botics.co",
+  api: "https://fancy_voice_30143Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
